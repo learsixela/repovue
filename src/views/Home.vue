13a16b347f52d9ff42a88f0062fc79f78434811f
israelpalma <ipalm001@gmail.com>
2020-08-29 12:18:07 -0400

    cambio de nombre a pagina titulo.vue por Pagina1.vue, para efectos de desplieguie de pagina

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <compo-footer
-    ></compo-footer>
   </div>
 
 </template>
@@ -11,13 +9,12 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
-import CompoFooter from '@/views/otros/Footer'
+
 
 export default {
   name: 'Home',
   components: {
     //HelloWorld,
-    'compo-footer': CompoFooter,
   }
 }
 </script>
